310d0dd453ad6d4f2b0fe1a2c867fdda3fa08373
Mahamadou <mmohmo@yahoo.com>
2023-11-08 22:12:55 -0500

    dropdown menu added

--- src/components/Header.js
+++ src/components/Header.js
@@ -90,8 +90,31 @@ export default function Header() {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12"> 
-              <div className="menu-bottom d-flex align-items-center">
-                <div></div>
+              <div className="menu-bottom d-flex align-items-center gap-30">
+                <div>
+                  <div className="dropdown">
+                    <Link>
+                      
+                    </Link>
+                    <button 
+                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center" 
+                      type="button" 
+                      data-bs-toggle="dropdown" 
+                      aria-expanded="false"
+
+                    >
+
+
+                    <span><i className="material-icons" style={{fontSize:"20px", color:"#ffffff"}}>dialpad</i></span>
+                      <span className="me-5 d-inline-block">Shop Categories</span>
+                    </button>
+                    <ul className="dropdown-menu">
+                      <li><a className="dropdown-item" href="#">Action</a></li>
+                      <li><a className="dropdown-item" href="#">Another action</a></li>
+                      <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </div>
+                </div>
                 <div className="menu-links">
                   <div className="d-flex align-items-center gap-15">
                     <NavLink to="/">Home</NavLink>
