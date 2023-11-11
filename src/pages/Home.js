65800d492a91d7459af39abd4dfaac456d3cd679
Mahamadou <mmohmo@yahoo.com>
2023-11-11 13:59:30 -0500

    home page first banner done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner postition-relative p-3">
+                <img 
+                  src="images/ecom-banner-img.jpeg" 
+                  alt="main banner"
+                  className="img-fluid rounded-3"
+                />
+                <div className="main-banner-content position-absolute">
+                  <h4>SUPERCHARGED FOR PROS.</h4>
+                  <h5>Special Sale</h5>
+                  <p>From $999.00 or $41.62/mo.<br/> for 24 mo. Footnote*</p>
+                  <Link className="button">BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
