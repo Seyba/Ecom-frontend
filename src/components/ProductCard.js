558441ef611cb6c43e68a4a9084243885d99c696
Mahamadou <mmohmo@yahoo.com>
2023-11-13 21:55:32 -0500

    react rating stars package loaded

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import ReactStars from "react-rating-stars-component"
 
 export default function ProductCard(){
   return (
@@ -10,6 +11,16 @@ export default function ProductCard(){
             </div>
             <div className="product-details">
                 <h6 className="brand">Havels</h6>
+                <h5 className="product-title">
+                    Sony EXTRA BASS Portable Splash-Proof Wireless Speaker
+                </h5>
+                <ReactStars 
+                    count={5} 
+                    size={24} 
+                    activeColor="#ffd700"
+                    value="3"
+                />
+                <p className="price">$220.00</p>
             </div>
         </div>
     </div>
