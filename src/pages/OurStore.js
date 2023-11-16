b6bb6237ef7d8ef180e35c5237c88080ed1a1102
Mahamadou <mmohmo@yahoo.com>
2023-11-15 21:03:20 -0500

    product tags section done

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -65,12 +65,81 @@ export default function OurStore() {
                                     </div>
                                         
                                 </div>
+                                <h5 className="sub-title">Colors</h5>
+                                <div>
+                                    <ul className="colors p-0">
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li> 
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li> 
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>      
+                                    </ul>
+                                </div>
+                                <h5 className="sub-title">Size</h5>
+                                <div className="form-check">
+                                    <input 
+                                        className="form-check-input"
+                                        type="checkbox" 
+                                        value="" 
+                                        id="color-1"
+                                    />
+                                    <label className="form-check-label" htmlFor="color-1">
+                                        S (12)
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input 
+                                        className="form-check-input"
+                                        type="checkbox" 
+                                        value="" 
+                                        id="color-2"
+                                    />
+                                    <label className="form-check-label" htmlFor="color-2">
+                                        M (10)
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input 
+                                        className="form-check-input"
+                                        type="checkbox" 
+                                        value="" 
+                                        id="color-3"
+                                    />
+                                    <label className="form-check-label" htmlFor="color-3">
+                                        L (12)
+                                    </label>
+                                </div>
                             </div>
                         </div>
                         <div className="filter-card mb-3">
                             <h3 className="filter-title">
                                 Product Tags
                             </h3>
+                            <div>
+                                <div className="product-tags d-flex flex-wrap align-items-center gap-10">
+                                    <span className="badge bg-light text-secondary rounded-3 py-2 px-2">Headphones</span>
+                                    <span className="badge bg-light text-secondary rounded-3 py-2 px-2">Laptop</span>
+                                    <span className="badge bg-light text-secondary rounded-3 py-2 px-2">Mobile</span>
+                                    <span className="badge bg-light text-secondary rounded-3 py-2 px-2">Oppo</span>
+                                    <span className="badge bg-light text-secondary rounded-3 py-2 px-2">Speaker</span>
+                                    <span className="badge bg-light text-secondary rounded-3 py-2 px-2">Tablet</span>
+                                    <span className="badge bg-light text-secondary rounded-3 py-2 px-2">Vivo</span>
+                                    <span className="badge bg-light text-secondary rounded-3 py-2 px-2">Wire</span>
+                                </div>
+                            </div>
                         </div>
                         <div className="filter-card mb-3">
                             <h3 className="filter-title">
