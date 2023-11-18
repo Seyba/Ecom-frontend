8c2c8524523c8e2dd78ea1cb4e2b6c5966b9e19d
Mahamadou <mmohmo@yahoo.com>
2023-11-18 18:56:00 -0500

    product details card added

--- src/pages/CompareProduct.js
+++ src/pages/CompareProduct.js
@@ -12,11 +12,43 @@ export const CompareProduct = () => {
                 <div className="row">
                     <div className="col-3">
                         <div className="compare-product-card position-relative">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg postion-absolute cross" viewBox="0 0 16 16">
-                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
-                            </svg>
+                            <div className="cross position-absolute">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                                </svg>
+                            </div>
+                            
                             <div className="product-card-image">
                                 <img src="/images/camera-1.jpg" alt="compare"/>
+                            </div>
+                            <div className="compare-product-details">
+                                <h5 className="title">Honor T1 7.0.1 GB RAM 8 ROM 7 Inc with Wi-fi+3G Tablet.</h5>
+                                <h6 className="price">$100</h6>
+                                <div>
+                                    <div className="product-detail">
+                                        <h5>Brand:</h5>
+                                        <p>Havels</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Type:</h5>
+                                        <p>Sony</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Availability:</h5>
+                                        <p>samsung</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Color:</h5>
+                                        <p>Dell</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Size:</h5>
+                                        <div className="d-flex gap-10">
+                                            <p>S</p>
+                                            <p>M</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
