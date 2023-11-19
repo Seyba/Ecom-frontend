a0c5bf7ea181fa4c65edb01a0e61fcaa965033ae
Mahamadou <mmohmo@yahoo.com>
2023-11-18 19:37:31 -0500

    product card loaded

--- src/pages/CompareProduct.js
+++ src/pages/CompareProduct.js
@@ -53,6 +53,133 @@ export const CompareProduct = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="col-3">
+                        <div className="compare-product-card position-relative">
+                            <div className="cross position-absolute">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                                </svg>
+                            </div>
+                            
+                            <div className="product-card-image">
+                                <img src="/images/bluetoothSpeaker-2.jpg" alt="compare"/>
+                            </div>
+                            <div className="compare-product-details">
+                                <h5 className="title">Sony A1 Portable Bluetooth Speaker With Microphone</h5>
+                                <h6 className="price mt-3">$200.00</h6>
+                                <div>
+                                    <div className="product-detail">
+                                        <h5>Brand:</h5>
+                                        <p>Sony</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Type:</h5>
+                                        <p>Bluetooth</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Availability:</h5>
+                                        <p>In Stock</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Color:</h5>
+                                        <Color/>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Size:</h5>
+                                        <div className="d-flex gap-10">
+                                            <p>S</p>
+                                            <p>M</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="compare-product-card position-relative">
+                            <div className="cross position-absolute">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                                </svg>
+                            </div>
+                            
+                            <div className="product-card-image">
+                                <img src="/images/headphone-2.jpg" alt="compare"/>
+                            </div>
+                            <div className="compare-product-details">
+                                <h5 className="title">Kids Headphones Bulk 10 Pack Multi Colored For Students</h5>
+                                <h6 className="price mt-3">$300.00</h6>
+                                <div>
+                                    <div className="product-detail">
+                                        <h5>Brand:</h5>
+                                        <p>Apple</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Type:</h5>
+                                        <p>Bluetooth</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Availability:</h5>
+                                        <p>In Stock</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Color:</h5>
+                                        <Color/>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Size:</h5>
+                                        <div className="d-flex gap-10">
+                                            <p>S</p>
+                                            <p>M</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="compare-product-card position-relative">
+                            <div className="cross position-absolute">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                                </svg>
+                            </div>
+                            
+                            <div className="product-card-image">
+                                <img src="/images/bluetoothSpeaker-1.jpg" alt="compare"/>
+                            </div>
+                            <div className="compare-product-details">
+                                <h5 className="title">Sony A1 Portable Bluetooth Speaker With Microphone</h5>
+                                <h6 className="price mt-3">$198.00</h6>
+                                <div>
+                                    <div className="product-detail">
+                                        <h5>Brand:</h5>
+                                        <p>Sony</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Type:</h5>
+                                        <p>Bluetooth</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Availability:</h5>
+                                        <p>In Stock</p>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Color:</h5>
+                                        <Color/>
+                                    </div>
+                                    <div className="product-detail">
+                                        <h5>Size:</h5>
+                                        <div className="d-flex gap-10">
+                                            <p>S</p>
+                                            <p>M</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </section>
