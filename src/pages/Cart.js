da4d36765e95cb893c2ac9e8797ef070be573209
Mahamadou <mmohmo@yahoo.com>
2023-11-26 17:40:18 -0500

    cart product added

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -19,7 +19,16 @@ export const Cart = () => {
                             <h4 className="cart-col-4">Total</h4>
                         </div>
                         <div className="cart-data py-3 d-flex justify-content-between align-content-center ">
-                            <div className="cart-col-1"></div>
+                            <div className="cart-col-1 gap-15 d-flex align-items-center">
+                                <div className="w-25">
+                                    <img src="/images/appleWatchSeries2.jpg" className="img-fluid" alt="watch"/>
+                                </div>
+                                <div className="w-75">
+                                    <h5 className="title">Smart Watch with Advanced Health Monitoring, Fitness Tracking</h5>
+                                    <p className="color">Color</p>
+                                    <p className="size">Size</p>
+                                </div>
+                            </div>
                             <div className="cart-col-2"></div>
                             <div className="cart-col-3"></div>
                             <div className="cart-col-4"></div>
