e57f7d102529578f107a3d3eae621649ffb2154b
Mahamadou <mmohmo@yahoo.com>
2023-11-25 21:55:48 -0500

    accordion heading styled

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -137,14 +137,14 @@ export const SingleProduct = () => {
                                     </div>
                                 </div>
                                 
-                                <div className="accordion">
+                                <div className="accordion accordion-wrapper">
                                     <div >
                                         <div className="d-flex justify-content-between">
                                             <div className="d-flex gap-15 align-items-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
                                                     <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456M12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
                                                 </svg>
-                                                <h5 className="mb-0">Shipping & Returns</h5>
+                                                <h5 className="mb-0 prod-accordion-title">Shipping & Returns</h5>
                                             </div>
                                             <div className="" id="headingOne">
                                                 <h5 className="mb-0">
@@ -160,7 +160,7 @@ export const SingleProduct = () => {
 
                                         <div id="collapseOne" className={`collapse ${showShippingDetails ? 'show ' : '' }`} aria-labelledby="headingOne" data-parent="#accordionExample">
                                             <div className="" >
-                                                <p className="prod_detail__data">
+                                                <p className="prod_detail__data px-3">
                                                     Free shipping and returns available on all orders!
                                                     We ship all US domestic orders within <b>5-10 business days!</b>
                                                 </p>
@@ -168,12 +168,12 @@ export const SingleProduct = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="accordion" >
+                                <div className="accordion accordion-wrapper" >
                                     <div >
                                         <div className="d-flex justify-content-between">
                                             <div className="d-flex gap-15 align-items-center">
                                                 <SiMaterialdesignicons/>
-                                                <h5 className="mb-0">Materials</h5>
+                                                <h5 className="mb-0 prod-accordion-title">Materials</h5>
                                             </div>
                                             <div className="" id="headingOne">
                                                 <h5 className="mb-0">
@@ -189,7 +189,7 @@ export const SingleProduct = () => {
 
                                         <div id="collapseOne" className={`collapse ${materialsInfo ? 'show ' : '' }`} aria-labelledby="headingOne" data-parent="#accordionExample">
                                             <div className="">
-                                                <p className="prod_detail__data">
+                                                <p className="prod_detail__data px-3">
                                                     Running Shoes cushions your stride with soft foam to keep you running in comfort. 
                                                     Lightweight knit material wraps your foot in breathable support, 
                                                     while a minimalist design fits in just about anywhere your day takes you.
@@ -198,14 +198,14 @@ export const SingleProduct = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="accordion">
+                                <div className="accordion accordion-wrapper">
                                     <div >
                                         <div className="d-flex justify-content-between">
                                             <div className="d-flex gap-15 align-items-center">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                                                 <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
                                             </svg>
-                                                <h5 className="mb-0">Care Instructions</h5>
+                                                <h5 className="mb-0 prod-accordion-title">Care Instructions</h5>
                                             </div>
                                             <div className="" id="headingOne">
                                                 <h5 className="mb-0">
@@ -221,7 +221,7 @@ export const SingleProduct = () => {
 
                                         <div id="collapseTwo" className={`collapse ${careDetails ? 'show ' : '' }`} aria-labelledby="headingOne" data-parent="#accordionExample">
                                             <div className="">
-                                                <p className="prod_detail__data">
+                                                <p className="prod_detail__data px-3">
                                                     Use a soft damp cloth and a drop of mild soap to remove any haze. Air dry.
                                                 </p>
                                             </div>
