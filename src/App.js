7e95b05bb5e98b8bc70641c5ce65da30357590fe
Mahamadou <mmohmo@yahoo.com>
2023-11-26 21:44:31 -0500

    checkout route added

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import { TermsAndConditions } from './pages/TermsAndConditions'
 import { RefundPolicy } from './pages/RefundPolicy'
 import { SingleProduct } from './pages/SingleProduct'
 import { Cart } from './pages/Cart'
+import { Checkout } from './pages/Checkout'
 
 function App() {
   return(
@@ -37,6 +38,7 @@ function App() {
             <Route path="/contact" element={<Contact/>}/>
             <Route path="/compare-product" element={<CompareProduct/>}/>
             <Route path="/cart" element={<Cart/>}/>
+            <Route path="/checkout" element={<Checkout/>}/>
             <Route path="/wishlist" element={<Wishlist/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/signup" element={<Signup/>}/>
