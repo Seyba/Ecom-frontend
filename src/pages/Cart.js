36b5aa47740b91f608519249f1fec18cf668849b
Mahamadou <mmohmo@yahoo.com>
2023-11-26 21:40:40 -0500

    cart page fully styled

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -58,6 +58,24 @@ export const Cart = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="col-12 py-2 mt-4">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div>
+                                <Link to='/product' className="button">Continue Shopping</Link>
+                                <p>Order special instructions</p>
+                                <textarea 
+                                    className="form-control"
+                                    name="intruction"
+                                />
+                            </div>
+                            <div className="d-flex flex-colum align-items-end">
+                                <h4>Subtotal</h4> $ 500.00
+                                <p>Taxes and shipping calculated at checkout</p>
+                                <Link className="button">Check Out</Link>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
