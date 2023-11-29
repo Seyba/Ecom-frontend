8d515433d494981e6fc45a56b0b7fea27d2618e0
Mahamadou <mmohmo@yahoo.com>
2023-11-29 15:32:21 -0500

    icon converted to img

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,26 +1,22 @@
 export const services = [
-    {
+    {   "img":"/images/car.png",
         "title": "Free Shipping",
         "tagline":"From all orders over $100"
     },
-    {
+    {   "img":"/images/gift.png",
         "title": "Daily Surprise Offers",
         "tagline":"Save up to 25%"
     },
-    {
-        "title": "Daily Surprise Offers",
-        "tagline":"Save up to 25%"
-    },
-    {
+    {   "img":"/images/headSet.png",
         "title": "Support 24/7",
         "tagline":"Shop with an expert"
     },
-    {
+    {   "img":"/images/tag.png",
         "title": "Affordable Prices",
         "tagline":"Get Factory Default Price"
     },
-    {
-        "title": "Affordable Prices",
+    {   "img":"/images/creditCard.png",
+        "title": "Secure Payment",
         "tagline":"100% Protected Payment"
     }
 ]
